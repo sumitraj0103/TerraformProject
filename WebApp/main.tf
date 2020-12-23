218abcdfab87c99e4d6c8b457357dd3aad27bc25
sumitraj0103 <srs@sitecore.net>
2020-12-23 23:17:58 +0800

    add

--- WebApp/main.tf
+++ WebApp/main.tf
@@ -1,5 +1,11 @@
 provider "azurerm" {
   features {}
+  version = "=2.4.0"
+
+  subscription_id = "27426494-41b4-4991-8c4b-c722fee829ac"
+  client_id       = "1f1b045e-08d8-4f50-9448-1d3b183fe989"
+  client_secret   = "Pumzy8SCVF399~~.._mik2hcoGhjjWs8U7"
+  tenant_id       = "91700184-c314-4dc9-bb7e-a411df456a1e"
 }
 
 resource "azurerm_resource_group" "main" {
